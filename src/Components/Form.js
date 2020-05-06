197be63f73f2f10c69221fb99dd8cc4154feb3e3
Wawarkar <payal.r.wawarkar@accenture.com>
2020-05-06 18:39:47 +0530

    added changes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,71 +1,63 @@
 import React, { useState, useEffect } from 'react';
-//import GetImage from './Components/Fetch';
 
-function FormData(){
+function FormData() {
     const [headerText, setHeaderText] = useState('');
     const [footerText, setFooterText] = useState('');
-    const [randomImg, setRandomImg] = useState('https://api.imgflip.com/get_memes');
+    const [randomImg, setRandomImg] = useState('');
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState('true');
+    const styles = {backgroundImage: `url(${randomImg})`};
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(headerText,'>>>>',footerText);
-        console.log(images.length,"images.length");
         const randNum = Math.floor(Math.random() * images.length);
         const randImage = images[randNum].url;
         setRandomImg(randImage);
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(response => {
-         const {images} = response.data;
-         console.log(images[0])
-             setImages([...images,
-                 {
-                   id: images.length,
-                   value: Math.random() * 100
-                 }
-             ]);
- 
+            .then(response => response.json())
+            .then(response => {
+               const images = response.data.memes;
+               setImages([...images,
+                {
+                    id: images.length,
+                    value: Math.random() * 100
+                }
+                ]);
         }).catch(() => setIsLoading(true))
-     
-    },[]);
-      
+    }, []);
 
-    return(
+
+    return (
         <div>
-        <form onSubmit = {handleSubmit}>
-            <label>
-            HeaderText:
-            <input 
-                type="text"
-                name = "headerText"
-                value = {headerText}
-                onChange = {event => setHeaderText(event.target.value)}
-            />
-            </label>
-            <label>
-            FooterText:
-            <input 
-                type="text"
-                name = "footerText"
-                value = {footerText}
-                onChange = {event => setFooterText(event.target.value)}
-            />
-            </label>
-           <button>Submit</button>
-        </form>
-        <div className="meme">
-         <img src={randomImg} alt=""></img>
-         <h2 className="top">{headerText}</h2>
-         <h2 className="bottom">{footerText}</h2>
-        </div>
+            <form onSubmit={handleSubmit}>
+             <input
+                        type="text"
+                        name="headerText"
+                        value={headerText}
+                        placeholder ="Enter HeaderText"
+                        onChange={event => setHeaderText(event.target.value)}
+                    />
+              &nbsp;&nbsp;&nbsp;
+            <input
+                        type="text"
+                        name="footerText"
+                        value={footerText}
+                        placeholder ="Enter FooterText"
+                        onChange={event => setFooterText(event.target.value)}
+                    />
+               &nbsp;&nbsp;&nbsp;
+                <button>Submit</button>
+                &nbsp;&nbsp;&nbsp;
+            </form>
+            <div className="meme"  style ={({backgroundImage: `url(${randomImg})`})}>
+                <h4 className="top-left">{headerText}</h4>
+                <h4 className="bottom-left">{footerText}</h4>
+            </div>
         </div>
     );
-
 }
 
 export default FormData;
